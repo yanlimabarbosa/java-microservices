940b6b61b955096b0e6b557d54c062bd726e0786
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-28 20:30:37 -0300

    feat: create InventoryController

---
+++ src/main/java/com/example/yan/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,28 @@
+package com.example.yan.inventoryservice.controller;
+
+import com.example.yan.inventoryservice.response.EventInventoryResponse;
+import com.example.yan.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InventoryController {
+
+    private InventoryService inventoryService;
+
+    @Autowired
+    public InventoryController(final InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/inventory/events")
+    public @ResponseBody List<EventInventoryResponse> inventoryGetAllEvents(){
+        return inventoryService.getAllEvents();
+    }
+}
