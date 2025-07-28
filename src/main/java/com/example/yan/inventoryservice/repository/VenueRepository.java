cf7a7b3d4f834d5a5305ec3a3a5d9f5b72de25d0
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-28 20:31:09 -0300

    feat: create Event and Venue repositories

---
+++ src/main/java/com/example/yan/inventoryservice/repository/VenueRepository.java
@@ -0,0 +1,9 @@
+package com.example.yan.inventoryservice.repository;
+
+import com.example.yan.inventoryservice.entity.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+}
