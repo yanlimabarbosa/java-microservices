4246ea7f7b18ac371c78a40004af39844554700d
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-28 20:31:18 -0300

    feat: create Event and Venue entities

---
+++ src/main/java/com/example/yan/inventoryservice/entity/Event.java
@@ -0,0 +1,31 @@
+package com.example.yan.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "venue")
+public class Event {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "total_capacity")
+    private Long totalCapacity;
+
+    @Column(name = "left_capacity")
+    private Long leftCapacity;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+}
---
+++ src/main/java/com/example/yan/inventoryservice/entity/Venue.java
@@ -0,0 +1,31 @@
+package com.example.yan.inventoryservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "venue")
+public class Venue {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "total_capacity")
+    private Long totalCapacity;
+}
