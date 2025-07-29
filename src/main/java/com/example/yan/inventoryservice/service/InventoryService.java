b5202559faee5f68ca3c2f31d58435ef82bb8856
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-28 21:00:45 -0300

    feat: create getAllEvents and getVenueInformation methods on InventoryService

--- src/main/java/com/example/yan/inventoryservice/service/InventoryService.java
+++ src/main/java/com/example/yan/inventoryservice/service/InventoryService.java
@@ -1,12 +1,16 @@
 package com.example.yan.inventoryservice.service;
 
+import com.example.yan.inventoryservice.entity.Event;
+import com.example.yan.inventoryservice.entity.Venue;
 import com.example.yan.inventoryservice.repository.EventRepository;
 import com.example.yan.inventoryservice.repository.VenueRepository;
 import com.example.yan.inventoryservice.response.EventInventoryResponse;
+import com.example.yan.inventoryservice.response.VenueInventoryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class InventoryService {
@@ -21,6 +25,22 @@ public class InventoryService {
     }
 
     public List<EventInventoryResponse> getAllEvents(){
+        final List<Event> events = eventRepository.findAll();
 
+        return events.stream().map(event-> EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .build()).collect(Collectors.toList());
+    }
+
+    public VenueInventoryResponse getVenueInformation(final Long venueId) {
+        final Venue venue = venueRepository.findById(venueId).orElse(null);
+
+        return VenueInventoryResponse.builder()
+                .venueId(venue.getId())
+                .venueName(venue.getName())
+                .totalCapacity(venue.getTotalCapacity())
+                .build();
     }
 }
