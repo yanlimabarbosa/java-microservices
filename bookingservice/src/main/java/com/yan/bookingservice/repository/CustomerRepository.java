df2096665ac5f9f199555576160f813b3028adc8
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-04 16:30:35 -0300

    feat: add CustomerRepository interface for customer data access

---
+++ bookingservice/src/main/java/com/yan/bookingservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.yan.bookingservice.repository;
+
+import com.yan.bookingservice.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
