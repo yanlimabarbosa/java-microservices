d2231038751526fd2c6461ca5b1a0d0783a4d935
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-04 16:29:10 -0300

    feat: update EventInventoryResponse to include eventId and ticketPrice fields

--- inventoryservice/src/main/java/com/example/yan/inventoryservice/response/EventInventoryResponse.java
+++ inventoryservice/src/main/java/com/example/yan/inventoryservice/response/EventInventoryResponse.java
@@ -6,12 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventInventoryResponse {
+    private Long eventId;
     private String event;
     private Long capacity;
     private Venue venue;
+    private BigDecimal ticketPrice;
 }
