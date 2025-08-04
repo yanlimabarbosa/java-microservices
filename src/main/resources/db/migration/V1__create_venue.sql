ef5de7aa66840addb305980421c1ef89c029d9c9
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-04 05:33:46 -0300

    feat: add SQL migration scripts for venue and event tables

---
+++ src/main/resources/db/migration/V1__create_venue.sql
@@ -0,0 +1,6 @@
+CREATE TABLE venue (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    total_capacity BIGINT NOT NULL
+); 
---
+++ src/main/resources/db/migration/V2__create_event.sql
@@ -0,0 +1,8 @@
+CREATE TABLE event (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    venue_id BIGINT NOT NULL,
+    total_capacity BIGINT NOT NULL,
+    left_capacity BIGINT NOT NULL,
+    CONSTRAINT fk_event_venue FOREIGN KEY (venue_id) references venue(id) ON DELETE CASCADE
+); 
