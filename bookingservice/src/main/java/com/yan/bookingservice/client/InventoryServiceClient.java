74c55d0f03080e406ac254763a215430788496a3
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-04 16:30:47 -0300

    feat: implement InventoryServiceClient to retrieve inventory data from external service

---
+++ bookingservice/src/main/java/com/yan/bookingservice/client/InventoryServiceClient.java
@@ -0,0 +1,17 @@
+package com.yan.bookingservice.client;
+
+import com.yan.bookingservice.response.InventoryResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class InventoryServiceClient {
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    public InventoryResponse getInventory(final Long eventId){
+        final RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(inventoryServiceUrl + "/event/" + eventId, InventoryResponse.class);
+    }
+}
