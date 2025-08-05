b328479cc6b910ded12ec584db555b3676676641
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-05 07:22:55 -0300

    feat: implement updateEventCapacity method in InventoryService to adjust event capacity and log changes

--- inventoryservice/src/main/java/com/example/yan/inventoryservice/service/InventoryService.java
+++ inventoryservice/src/main/java/com/example/yan/inventoryservice/service/InventoryService.java
@@ -6,6 +6,7 @@ import com.example.yan.inventoryservice.repository.EventRepository;
 import com.example.yan.inventoryservice.repository.VenueRepository;
 import com.example.yan.inventoryservice.response.EventInventoryResponse;
 import com.example.yan.inventoryservice.response.VenueInventoryResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class InventoryService {
 
     private final EventRepository eventRepository;
@@ -56,5 +58,12 @@ public class InventoryService {
                 .venue(event.getVenue())
                 .ticketPrice(BigDecimal.valueOf(event.getTicketPrice()))
                 .build();
+    }
+
+    public void updateEventCapacity(final Long eventId, final Long ticketsBooked) {
+        final Event event = eventRepository.findById(eventId).orElse(null);
+        event.setLeftCapacity(event.getLeftCapacity() - ticketsBooked);
+        eventRepository.saveAndFlush(event);
+        log.info("Updated event capacity for event id: {} with tickets booked: {}", eventId, ticketsBooked);
     }
 }
