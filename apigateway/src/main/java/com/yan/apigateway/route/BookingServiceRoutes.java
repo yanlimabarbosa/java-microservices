f3dc83cb1ef4deb9ca725d606ad52f1fa22be748
Yan Lima Barbosa <yanbr763@gmail.com>
2025-08-09 02:17:31 -0300

    feat: add circuit breaker and fallback route to BookingServiceRoutes for improved resilience

--- apigateway/src/main/java/com/yan/apigateway/route/BookingServiceRoutes.java
+++ apigateway/src/main/java/com/yan/apigateway/route/BookingServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.yan.apigateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.*;
+
+import java.net.URI;
 
 @Configuration
 public class BookingServiceRoutes {
@@ -13,6 +17,17 @@ public class BookingServiceRoutes {
         return GatewayRouterFunctions.route("booking-service")
                 .route(RequestPredicates.POST("/api/v1/booking"),
                         HandlerFunctions.http("http://localhost:8081/api/v1/booking"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .POST("/fallbackRoute",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Booking service is down"))
                 .build();
     }
 }
